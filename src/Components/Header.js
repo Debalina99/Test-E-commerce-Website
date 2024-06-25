8d9bcd1360ae3e8e78e0cab4ed13d8c02c213ace
Debalina99 <debalinalaha13630@gmail.com>
2024-06-25 18:53:51 +0530

    Few changes in style

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,27 +8,27 @@ return(
             <p>Grocery</p>
         </div>
         <div className="header-card">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBM0E3ihdF_j151idqOLvbMHPLbj5AvI0BbQ&s" alt="grocery"></img>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBM0E3ihdF_j151idqOLvbMHPLbj5AvI0BbQ&s" alt="mobiles"></img>
             <p>Mobiles</p>
         </div>
         <div className="header-card">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLP1d7s00ziEfRhq4Jmtxy-C19ct3eQxFAxw&s" alt="grocery"></img>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLP1d7s00ziEfRhq4Jmtxy-C19ct3eQxFAxw&s" alt="fashion"></img>
             <p>Fashion</p>
         </div>
         <div className="header-card">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGbYc77x0s4d14YIDswewbUxkLhQzxILXesFuT1FXB3iij-HqGLopHOfPHYcoAw2rfHhM&usqp=CAU" alt="grocery"></img>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGbYc77x0s4d14YIDswewbUxkLhQzxILXesFuT1FXB3iij-HqGLopHOfPHYcoAw2rfHhM&usqp=CAU" alt="electronics"></img>
             <p>Electronics</p>
         </div>
         <div className="header-card">
-            <img src="https://5.imimg.com/data5/VG/VU/SL/SELLER-26317611/teak-wood-almirah-500x500.jpg" alt="grocery"></img>
+            <img src="https://5.imimg.com/data5/VG/VU/SL/SELLER-26317611/teak-wood-almirah-500x500.jpg" alt="furniture"></img>
             <p> Furniture</p>
         </div>
         <div className="header-card">
-            <img src="https://e0.pxfuel.com/wallpapers/1/36/desktop-wallpaper-five-home-appliances-that-consume-electricity-the-most-home-appliances-outdoor-kitchen-appliances-gas-cooker.jpg" alt="grocery"></img>
+            <img src="https://e0.pxfuel.com/wallpapers/1/36/desktop-wallpaper-five-home-appliances-that-consume-electricity-the-most-home-appliances-outdoor-kitchen-appliances-gas-cooker.jpg" alt="appliances"></img>
             <p>Appliances</p>
         </div>
         <div className="header-card">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIWTqn1nH4rzzsXfeLtbIQBTSjVyvegDAgdiCiwSKMH81soNMOz70e1CACoLnUP_3GLTw&usqp=CAU" alt="grocery"></img>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIWTqn1nH4rzzsXfeLtbIQBTSjVyvegDAgdiCiwSKMH81soNMOz70e1CACoLnUP_3GLTw&usqp=CAU" alt="travel"></img>
             <p>Travel</p>
         </div>
         <div className="header-card">
@@ -38,6 +38,14 @@ return(
         <div className="header-card">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI3NxbDIL5NycyCT3lNAZy0_O01p05cbWcpsortGzk6kYMVHcy636N2Cgqh9xgA5wyGjA&usqp=CAU" alt="two wheelers"></img>
             <p>Two Wheelers</p>
+        </div>
+        <div className="header-card">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhEvUvh5X9J9bVPoV1tf79mgu009DRLHUQdg&s" alt="Food"></img>
+            <p> Food</p>
+        </div>
+        <div className="header-card">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxGyqItc37fMxODZjHvbcLfZ2xDaiN-NTEfA&s" alt="Healtcare"></img>
+            <p>Healtcare</p>
         </div>
       </div>
     )
